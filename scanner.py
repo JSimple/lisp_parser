1630c350a230db679e30370ad71ca271a9580764
JSimple <josephinemayasimple@gmail.com>
2022-02-16 00:35:44 -0500

    add: Token class

--- scanner.py
+++ scanner.py
@@ -1,3 +1,10 @@
 class Token:
     def __init__(self, type, lexeme, literal, line ) -> None:
-        
+        self.type = type
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
+    
+    def toString(self):
+        return f"{self.type} {self.lexeme} {self.literal}"
+    
